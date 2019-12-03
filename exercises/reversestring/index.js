12559804a7b45247a8a6547a9cb791e2b2d1fb04
Maria Obaturova <m.obaturova@gmail.com>
2019-12-03 02:36:43 -0500

    feat: reversed string solution - brute force

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let returnedStr = '';
+    for (let i = str.length-1; i >= 0; i--){
+        returnedStr += str[i];
+    }
+    return returnedStr;
+}
 
 module.exports = reverse;
