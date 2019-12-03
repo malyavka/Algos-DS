877a41d804ef0ce6be03ac058131252daf015e56
Maria Obaturova <m.obaturova@gmail.com>
2019-12-03 02:54:01 -0500

    feat: reversed string solution - built-in methods

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -5,13 +5,20 @@
 //   reverse('apple') === 'leppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
-
+/** Solution with FOR LOOP
 function reverse(str) {
     let returnedStr = '';
     for (let i = str.length-1; i >= 0; i--){
         returnedStr += str[i];
     }
     return returnedStr;
+}
+**/
+function reverse(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
 }
 
 module.exports = reverse;
