2f7a62c59076c4362c2894390d0fcce575f41f4c
Maria Obaturova <m.obaturova@gmail.com>
2019-12-05 16:44:48 -0500

    feat: reverseInt solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,18 +7,35 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+/** Naive solution
+ **/
+// function reverseInt(n) {
+//     let str = n
+//         .toString()
+//         .split('')
+//         .reverse()
+//     if (str[0] === '0' && str.length > 1) str = str.slice(1);
+//     if (str[str.length-1]  === '-') {
+//         str.unshift(str.pop())
+//     }
+//     return parseInt(str.join(''));
+//
+// }
 
 function reverseInt(n) {
     let str = n
         .toString()
         .split('')
         .reverse()
-    if (str[0] === '0' && str.length > 1) str = str.slice(1);
-    if (str[str.length-1]  === '-') {
-        str.unshift(str.pop())
-    }
-    return parseInt(str.join(''));
+        .join('');
 
+    /**
+     * The Math.sign() function returns either a positive or negative +/- 1,
+     * indicating the sign of a number passed into the argument.
+     * If the number passed into Math.sign() is 0, it will return a +/- 0.
+     * Note that if the number is positive, an explicit (+) will not be returned.
+     */
+    return parseInt(str) * Math.sign(n);
 }
 
 module.exports = reverseInt;
