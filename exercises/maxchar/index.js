cd5ce783dfc39966bf1f19c59ee253c16052c75e
Maria Obaturova <m.obaturova@gmail.com>
2019-12-05 22:22:51 -0500

    feat: maxChar solution added

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const counter = {};
+    for (let i = 0; i < str.length; i++)
+    {
+        if (counter[str[i]]){
+            counter[str[i]]++;
+        }
+        else {
+            counter[str[i]] = 1;
+        }
+    }
+    let max = 0;
+    let returnedChar = '';
+    for (let key in counter) {
+        if (counter[key] > max) {
+            max = counter[key];
+            returnedChar = key;
+        }
+    }
+    return returnedChar;
+}
+
 
 module.exports = maxChar;
