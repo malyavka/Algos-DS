8a203bf000e31e36c1e394f604b238fdbd7e2479
Maria Obaturova <m.obaturova@gmail.com>
2019-12-06 00:19:05 -0500

    feat: anagrams added for loop solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const counter = {};
+    stringA = stringA
+        .toLowerCase()
+        .replace(/[^\w]/g, '');
+    stringB = stringB
+        .toLowerCase()
+        .replace(/[^\w]/g, '');
+
+    for (let i = 0; i < stringA.length; i++) {
+        if (counter[stringA[i]])
+        {
+            counter[stringA[i]]++;
+        }
+        else
+        {
+            counter[stringA[i]] = 1;
+        }
+    }
+    for (let i = 0; i < stringB.length; i++) {
+        if (counter[stringB[i]])
+        {
+            counter[stringB[i]]--;
+        }
+        else
+        {
+            counter[stringA[i]] = 1;
+        }
+    }
+    for (let key in counter) {
+        if (counter[key] !== 0) {
+            return false;
+        }
+    }
+    return true;
+
+}
 
 module.exports = anagrams;
