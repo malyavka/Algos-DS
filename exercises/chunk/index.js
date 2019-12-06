5223578d0f584f58b0b3389d4846af8a6370d20a
Maria Obaturova <m.obaturova@gmail.com>
2019-12-05 23:55:32 -0500

    feat: chunk functions - while loop and index solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,16 +7,30 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+//
 
-function chunk(array, size) {
-    let result = [];
-    let sub = [];
-    for (let i = 0; i <= array.length; i++) {
-        if (sub.length === size || i === array.length) {
-            result.push(sub);
-            sub = []
-        }
-        sub.push(array[i])
+// NAIVE
+// function chunk(array, size) {
+//     let result = [];
+//     let sub = [];
+//     for (let i = 0; i <= array.length; i++) {
+//         if (sub.length === size || i === array.length) {
+//             result.push(sub);
+//             sub = []
+//         }
+//         sub.push(array[i])
+//     }
+//
+//     return result;
+// }
+
+function chunk (array, size) {
+    let index = 0;
+    const result = [];
+
+    while (index < array.length) {
+        result.push(array.slice(index, index+size));
+        index += size;
     }
 
     return result;
