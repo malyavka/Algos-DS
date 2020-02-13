f7b657f8a535a323468f7f005ad43152794ce0e5
Maria Obaturova <m.obaturova@gmail.com>
2020-02-13 06:59:51 -0500

    feat(recursion): added exponent solution

---
+++ exercises/challenges/recursion.js
@@ -0,0 +1,11 @@
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exponent){
+    if (exponent === 0) {
+        return 1;
+    }
+    exponent--;
+    return base * power(base,exponent )
+}
