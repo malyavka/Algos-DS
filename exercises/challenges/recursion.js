5be6767674efc95b918b0fcb902d3cd8f8c9eeb3
Maria Obaturova <m.obaturova@gmail.com>
2020-02-17 07:00:21 -0500

    feat(recursion): added factorial  solution

--- exercises/challenges/recursion.js
+++ exercises/challenges/recursion.js
@@ -9,3 +9,16 @@ function power(base, exponent){
     exponent--;
     return base * power(base,exponent )
 }
+
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(num){
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorial(num-1)
+
+}
