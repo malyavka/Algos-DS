1f0e6a80b23511410c335d1efc48d2763f879745
Maria Obaturova <m.obaturova@gmail.com>
2020-02-19 07:03:01 -0500

    fix(fib): added 2nd solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,11 +8,27 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function fib3(n) {
     if (n < 2) {
         return n;
     }
     return fib (n-1) + fib(n-2)
 }
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
 
+function fib(num){
+    // add whatever parameters you deem necessary - good luck!
+    if (num === 1) {
+        return 1
+    }
+    else if (num === 2) {
+        return 1
+    } else {
+        return fib(num-1) + fib(num-2)
+
+    }
+}
 module.exports = fib;
