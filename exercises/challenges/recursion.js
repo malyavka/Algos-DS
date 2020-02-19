0b0b5532f5cb375e95e208838b9b9a364d0634b3
Maria Obaturova <m.obaturova@gmail.com>
2020-02-19 07:01:33 -0500

    feat(recursion): added recursiveRange solution

--- exercises/challenges/recursion.js
+++ exercises/challenges/recursion.js
@@ -32,3 +32,15 @@ function productOfArray(arr) {
     }
     return arr.pop() * productOfArray(arr)
 }
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+function recursiveRange(num){
+    if (num === 0) {
+        return 0;
+    }
+    return num + recursiveRange(num-1)
+}
+
